4bfd00e0aed748c0fa43be386ed29047357baffc
Manavatelemensis <168632844+Manavatelemensis@users.noreply.github.com>
2024-05-21 07:21:43 +0000

    Aggregation query Practise

---
+++ Aggregation.js
@@ -0,0 +1,90 @@
+// 1. Find the Number Of Active User
+
+/*
+
+[
+    //First Stage Where we Find the number of 
+    {
+  $match: {
+    isActive:true
+  }
+},
+ 
+Second Stage 
+{
+  $count: 'ActiveUser'
+}
+]
+
+
+*/
+
+
+// 2. What is the average age of all users
+
+
+/*
+
+[{
+  $group: {
+    _id: null,
+    averageAge: {
+      $avg : "$age"
+    }
+  }
+}] 
+
+*/
+
+
+// 3. List The Top 2 most common fav fruit
+
+/*
+
+
+[
+    First Stage to Group All Fruit and count the number of users fav fruit
+{
+  $group: {
+    _id:"$favoriteFruit",
+    countNumber:{
+      $sum:1
+    }
+    }
+},
+
+   Second Stage to sort the number of fruit
+ {
+  $sort: {
+    countNumber:-1
+  }
+},
+    Third Stage to Show the top 2
+ {
+   $limit: 2
+ }
+] 
+
+
+*/
+
+
+// Total Number Of Male and Female
+
+
+/*
+
+[{
+  $group: {
+    _id: "$gender",
+    countGender:{
+      $sum:1
+    }
+  }
+}]
+
+*/
+
+
+// Names and age of users who are inactive  aand have velit  as a tag
+
