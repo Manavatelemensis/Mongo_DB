3bc77c95046109455714adead7f1ad16035f64ab
Manavatelemensis <168632844+Manavatelemensis@users.noreply.github.com>
2024-05-23 07:22:49 +0000

    Query 1 Decoded

--- Notes.js
+++ Notes.js
@@ -82,3 +82,29 @@
 // Querying by data type is useful when dealing with highly unstructured data where data types are not predictable.
 
 //{ field: { $type: <BSON type> } }
+
+
+
+
+
+// json DataT
+// [
+// {
+// "address": {
+// "building": "1007",
+// "coord": [ -73.856077, 40.848447 ],
+// "street": "Morris Park Ave",
+// "zipcode": "10462"
+// },
+// "borough": "Bronx",
+// "cuisine": "Bakery",
+// "grades": [
+// { "date": { "$date": 1393804800000 }, "grade": "A", "score": 2 },
+// { "date": { "$date": 1378857600000 }, "grade": "A", "score": 6 },
+// { "date": { "$date": 1358985600000 }, "grade": "A", "score": 10 },
+// { "date": { "$date": 1322006400000 }, "grade": "A", "score": 9 },
+// { "date": { "$date": 1299715200000 }, "grade": "B", "score": 14 }
+// ],
+// "name": "Morris Park Bake Shop",
+// "restaurant_id": "30075445"
+// }]
